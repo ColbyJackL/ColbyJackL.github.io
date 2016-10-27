07cb752b09a956a6605518ce6b328d41ee408936
Colby Lorch <Colby@Colbys-MacBook-Air.local>
2016-10-27 15:36:23 -0600

    Did Major work.

--- 340_InteractiveMedia/HW_7_2/pacman.js
+++ 340_InteractiveMedia/HW_7_2/pacman.js
@@ -1,136 +1,156 @@
 function Pacman(canvX, canvY, pacmanDiameter) {
 
-  this.canvasWidth = canvX;
-  this.canvasHeight = canvY;
 
-  this.pDiam = pacmanDiameter;
-  this.pXPos = random(this.pDiam / 2, canvX - this.pDiam / 2); // position of pacman
-  this.pYPos = random(this.pDiam / 2, canvY - this.pDiam / 2);
-  this.startAngle = .3; // angle of pacman
-  this.stopAngle = -.3;
-  this.eyeX = -this.pDiam / 8; // pacman eye position
-  this.eyeY = -this.pDiam / 4;
+    this.wait = false;
+    this.canvasWidth = canvX;
+    this.canvasHeight = canvY;
 
-  this.pacXDirec = 1; // direction of movement
-  this.pacYDirec = 0;
-  this.pacSpeed = 1; // speed
+    this.pDiam = pacmanDiameter;
+    this.pXPos = random(this.pDiam / 2, canvX - this.pDiam / 2); // position of pacman
+    this.pYPos = random(this.pDiam / 2, canvY - this.pDiam / 2);
+    this.startAngle = QUARTER_PI; // angle of mouth
+    this.stopAngle = -QUARTER_PI;
+    this.eyeX = -this.pDiam / 8; // pacman eye position
+    this.eyeY = -this.pDiam / 4;
 
-  this.secTime = 0; // timer variables
-  this.secTimeMax = 240; // 240 frames is four seconds
-  this.timeGoal = random(this.secTimeMax + 1);
-  this.dirCount = 0;
+    this.pacXDirec = 1; // direction of movement
+    this.pacYDirec = 0;
+    this.pacSpeed = 1; // speed
 
-  this.startAngleChange = -1; // variables to move the mouth
-  this.stopAngleChange = 1;
+    this.secTime = 0; // timer variables
+    this.secTimeMax = 240; // 240 frames is four seconds
+    this.timeGoal = random(this.secTimeMax + 1);
+    this.dirCount = 0;
+
+    this.startAngleChange = -1; // variables to move the mouth
+    this.stopAngleChange = 1;
+    this.mouthChange = 0;
+    this.mouthDelta = 0.01;
 }
 
 Pacman.prototype.drawPacman = function() {
-  push();
-  translate(this.pXPos, this.pYPos); // set pacman position
-  fill("rgb(255, 214, 0)"); // Pacman Fill color
-  arc(0, 0, this.pDiam, this.pDiam, this.startAngle, this.stopAngle, PIE); // make pacman based on variables
-  fill(0);
-  ellipse(this.eyeX, this.eyeY, this.pDiam / 9, this.pDiam / 9);
-  pop();
+    push();
+    translate(this.pXPos, this.pYPos); // set pacman position
+    fill("rgb(255, 214, 0)"); // Pacman Fill color
+    arc(0, 0, this.pDiam, this.pDiam, this.startAngle - this.mouthChange, this.stopAngle + this.mouthChange, PIE); // make pacman based on variables
+    fill(0);
+    ellipse(this.eyeX, this.eyeY, this.pDiam / 9, this.pDiam / 9);
+    pop();
 }
 
 Pacman.prototype.movePacman = function() {
-  this.pXPos += this.pacXDirec * this.pacSpeed; //moves pacman based on direction and speed
-  this.pYPos += this.pacYDirec * this.pacSpeed;
+  if (this.wait) {
+    // do nothing
+  } else {
+    this.pXPos += this.pacXDirec * this.pacSpeed; //moves pacman based on direction and speed
+    this.pYPos += this.pacYDirec * this.pacSpeed;
+  }
+  //Upate Mouth position
+  if (this.mouthChange > PI/4 - 0.01 ) {
+    this.mouthDelta = this.mouthDelta * -1;
+    this.mouthChange = PI/4 - 0.01;
+  } else if ( this.mouthChange <= 0 ) {
+      this.mouthDelta = this.mouthDelta * -1;
+      this.mouthChange = 0.001;
+  }
+  this.mouthChange += this.mouthDelta * this.pacSpeed
+
   this.wallCollision();
 }
 
+
+
+
 Pacman.prototype.northPacman = function() { // make pacman go north
-  this.startAngle = -QUARTER_PI; // set angles for pacman direction
-  this.stopAngle = PI + QUARTER_PI;
-  this.eyeX = -this.pDiam / 4; // set position of eye
-  this.eyeY = this.pDiam / 8;
-  this.pacXDirec = 0; // set movement direction
-  this.pacYDirec = -1;
-  this.pacSpeed = random(1, 6);
+    this.startAngle = -QUARTER_PI; // set mouth direction
+    this.stopAngle = PI + QUARTER_PI;
+    this.eyeX = -this.pDiam / 4; // set position of eye
+    this.eyeY = this.pDiam / 8;
+    this.pacXDirec = 0; // set movement direction
+    this.pacYDirec = -1;
+    this.pacSpeed = random(1, 6);
 }
 
 Pacman.prototype.southPacman = function() { // make pacman go south
-  this.startAngle = HALF_PI + QUARTER_PI;
-  this.stopAngle = QUARTER_PI;
-  this.eyeX = this.pDiam / 4;
-  this.eyeY = -this.pDiam / 8;
-  this.pacXDirec = 0;
-  this.pacYDirec = 1;
-  this.pacSpeed = random(1, 6);
+    this.startAngle = HALF_PI + QUARTER_PI;
+    this.stopAngle = QUARTER_PI;
+    this.eyeX = this.pDiam / 4;
+    this.eyeY = -this.pDiam / 8;
+    this.pacXDirec = 0;
+    this.pacYDirec = 1;
+    this.pacSpeed = random(1, 6);
 }
 
 Pacman.prototype.westPacman = function() { // make pacman go west
-  this.startAngle = PI + QUARTER_PI;
-  this.stopAngle = PI - QUARTER_PI;
-  this.eyeX = this.pDiam / 8;
-  this.eyeY = -this.pDiam / 4;
-  this.pacXDirec = -1;
-  this.pacYDirec = 0;
-  this.pacSpeed = random(1, 6);
+    this.startAngle = PI + QUARTER_PI;
+    this.stopAngle = PI - QUARTER_PI;
+    this.eyeX = this.pDiam / 8;
+    this.eyeY = -this.pDiam / 4;
+    this.pacXDirec = -1;
+    this.pacYDirec = 0;
+    this.pacSpeed = random(1, 6);
 }
 
 Pacman.prototype.eastPacman = function() { // make pacman go east
-  this.startAngle = QUARTER_PI;
-  this.stopAngle = -QUARTER_PI;
-  this.eyeX = -this.pDiam / 8;
-  this.eyeY = -this.pDiam / 4;
-  this.pacXDirec = 1;
-  this.pacYDirec = 0;
-  this.pacSpeed = random(1, 6);
+    this.startAngle = QUARTER_PI;
+    this.stopAngle = -QUARTER_PI;
+    this.eyeX = -this.pDiam / 8;
+    this.eyeY = -this.pDiam / 4;
+    this.pacXDirec = 1;
+    this.pacYDirec = 0;
+    this.pacSpeed = random(1, 6);
 }
 
 Pacman.prototype.timer = function() { // creates a timer
-  if (this.secTime >= this.timeGoal) {
-    this.changeDir(); // calls function when timer elapses
-    this.secTime = 0; // resets timer
-    this.timeGoal = random(0, this.secTimeMax + 1); // resetst random timer goal
-  } else {
-    this.secTime++; //increments timer
-  }
+    if (this.secTime >= this.timeGoal) {
+        this.changeDir(); // calls function when timer elapses
+        this.secTime = 0; // resets timer
+        this.timeGoal = random(0, this.secTimeMax + 1); // resetst random timer goal
+    } else {
+        this.secTime++; //increments timer
+    }
 }
 
 Pacman.prototype.wallCollision = function() {
-  if (this.pXPos >= this.canvasWidth - this.pDiam / 2) {
-    this.stopMoving;
-    this.pXPos = this.canvasWidth - this.pDiam / 2; //pacman stays in canvas
-  } else if (this.pXPos <= this.pDiam / 2) {
-    this.stopMoving;
-    this.pXPos = this.pDiam / 2;
-  }
-  if (this.pYPos >= this.canvasHeight - this.pDiam / 2) {
-    this.stopMoving;
-    this.pYPos = this.canvasHeight - this.pDiam / 2; //pacman stays in canvas
-  } else if (this.pYPos <= this.pDiam / 2) {
-    this.stopMoving;
-    this.pYPos = this.pDiam / 2;
-  }
+    if (this.pXPos >= this.canvasWidth - this.pDiam / 2) {
+        this.stopMoving;
+        this.pXPos = this.canvasWidth - this.pDiam / 2; //pacman stays in canvas
+    } else if (this.pXPos <= this.pDiam / 2) {
+        this.stopMoving;
+        this.pXPos = this.pDiam / 2;
+    }
+    if (this.pYPos >= this.canvasHeight - this.pDiam / 2) {
+        this.stopMoving;
+        this.pYPos = this.canvasHeight - this.pDiam / 2; //pacman stays in canvas
+    } else if (this.pYPos <= this.pDiam / 2) {
+        this.stopMoving;
+        this.pYPos = this.pDiam / 2;
+    }
 }
 
-Pacman.prototype.stopMoving = function() { // Pacman stops
-  this.pacXDirec = 0;
-  this.pacYDirec = 0;
-}
+
 
 Pacman.prototype.changeDir = function() {
-  var newDirCount;
+    var newDirCount;
 
-  do {
-    newDirCount = floor(random(4)); // pacman direction does not repeat
-  } while (newDirCount == this.DirCount);
+    do {
+        newDirCount = floor(random(4)); // pacman direction does not repeat
+    } while (newDirCount == this.dirCount);
 
-  this.DirCount = newDirCount;
+    this.dirCount = newDirCount;
 
-  if (newDirCount === 0) { // Call function for newdirection
-    this.eastPacman();
-  } else if (newDirCount === 1) {
-    this.westPacman();
-  } else if (newDirCount === 2) {
-    this.northPacman();
-  } else if (newDirCount === 3) {
-    this.southPacman();
-  }
-  function pacmanCollide() {
+    if (newDirCount === 0) { // Call function for newdirection
+        this.eastPacman();
+    } else if (newDirCount === 1) {
+        this.westPacman();
+    } else if (newDirCount === 2) {
+        this.northPacman();
+    } else if (newDirCount === 3) {
+        this.southPacman();
+    }
+}
+
+Pacman.prototype.pacmanCollide = function(pacArr, myIdx) {
     var pacmanX = 0;
     var pacmanY = 0;
     var pacmanDistance = 0;
@@ -138,27 +158,28 @@ Pacman.prototype.changeDir = function() {
     var pacmanYDirection = 0;
     var pacmanDiam = 0;
 
+    this.wait=false;
+    for (var i = 0; i < pacArr.length; i++) {
+        if (i != myIdx) {
 
-    for (var i = 0; i < pacmen.length; i++) {
-      pacmanX = pacmen[i].pXPos;
-      pacmanY = pacmen[i].pYPos;
-      pacmanXDirection = pacmen[i].pacXDirec;
-      pacmanYDirection = pacmen[i].pacYDirec;
-      pacmanDiam = pacmen[i].pDiam;
+            otherX = pacArr[i].pXPos;
+            otherY = pacArr[i].pYPos;
+            otherXDirection = pacArr[i].pacXDirec;
+            otherYDirection = pacArr[i].pacYDirec;
+            combDiam = pacArr[i].pDiam/2 + this.pDiam/2 + this.pacSpeed;
 
-      for (var j = 0; j < pacmen.length; j++) {
-        pacmanDistance = dist(pacmanX, pacmanY, pacmen[j].pXPos, pacmen[j].pYPos);
-        if (pacmanDistance <= pacmanDiam && pacmen[j].pXPos > pacmanX && pacmanXDirection == 1) {
-          pacmen[i].stopMoving(); // checks for collision to the right
-          } else if (pacmanDistance <= pacmanDiam && pacmen[j].pXPos < pacmanX && pacmanXDirection == -1) {
-            pacmen[i].stopMoving(); // check for collision to the left
-          } else if (pacmanDistance <= pacmanDiam && pacmen[j].pYPos > pacmanY && pacmanYDirection == 1) {
-            pacmen[i].stopMoving(); // check for collision below
-          } else if (pacmanDistance <= pacmanDiam && pacmen[j].pYPos < pacmanY && pacmanYDirection == -1) {
-            pacmen[i].stopMoving(); //checks for collison above
+
+            pacmanDistance = dist(this.pXPos, this.pYPos, otherX, otherY);
+            if (pacmanDistance <= combDiam && this.pXPos > otherX && this.dirCount == 1 ) {
+                this.wait = true; // checks for collision to the right
+            } else if (pacmanDistance <= combDiam && this.pXPos < otherX && this.dirCount == 0 ) {
+                this.wait = true; // check for collision to the left
+            } else if (pacmanDistance <= combDiam && this.pYPos > otherY  && this.dirCount == 2) {
+                this.wait = true; // check for collision below
+            } else if (pacmanDistance <= combDiam && this.pYPos < otherY  && this.dirCount == 3) {
+                this.wait = true; //checks for collison above
+            }
         }
-      }
-
     }
-}
+
 }
--- 340_InteractiveMedia/HW_7_2/sketch.js
+++ 340_InteractiveMedia/HW_7_2/sketch.js
@@ -1,10 +1,11 @@
 var pacmen = [];
+var numPacmen = 10;
 
 function setup() {
   frameRate(60);
   createCanvas(1000, 600);
 
-  for (var i = 0; i < 7; i++) { //changes number of pacmen
+  for (var i = 0; i < numPacmen; i++) { //changes number of pacmen
     pacmen[i] = new Pacman(width, height, 40);
   }
 }
@@ -13,10 +14,12 @@ function draw() {
   background(0);
 
   for (var i = 0; i < pacmen.length; i++) {
-    pacmen[i].drawPacman();
+
+    pacmen[i].pacmanCollide( pacmen, i );
     pacmen[i].movePacman();
     pacmen[i].timer();
-    //pacmen[i].pacmanCollide();
+    pacmen[i].drawPacman();
+
   }
 
 }
